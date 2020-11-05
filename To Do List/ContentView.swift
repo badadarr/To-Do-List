6e17f8af36d5ce84ccbf7a3f6f5f04cc9eb66418
Badar <badar.maulana.techno@gmail.com>
2020-11-05 16:03:46 +0700

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by badar on 05/11/20.
+//  Copyright © 2020 badar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
